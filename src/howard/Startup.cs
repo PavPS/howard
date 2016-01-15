92e85bc2fa321eb18b01a77d9bede49765ceede5
PavPS <ipavps@gmail.com>
2016-01-15 14:16:49 -0800

    another comment remove

--- src/howard/Startup.cs
+++ src/howard/Startup.cs
@@ -54,7 +54,6 @@ namespace howard
             services.AddTransient<ISmsSender, AuthMessageSender>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
