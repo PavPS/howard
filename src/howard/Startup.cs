ed6cec8ec15b986c4f55b7ad1c2e6b5a9375b143
PavPS <ipavps@gmail.com>
2016-01-15 14:15:19 -0800

    Comment remove

--- src/howard/Startup.cs
+++ src/howard/Startup.cs
@@ -35,7 +35,6 @@ namespace howard
 
         public IConfigurationRoot Configuration { get; set; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
