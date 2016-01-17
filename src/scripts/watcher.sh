6165f412d86d2cc18b689378a1fe7f5f0da3ec57
pavps <donot@email.me>
2016-01-17 19:42:14 +0000

    script updated

--- src/scripts/watcher.sh
+++ src/scripts/watcher.sh
@@ -2,10 +2,15 @@
 
 while true 
 do
-  echo 'Wating for script file change'
-  inotifywait -qe close_write ./../howard/wwwroot/scripts/pi.py
-  echo 'Wait complete. Executing command'
-  sudo python ./../howard/wwwroot/scripts/pi.py
-  echo 'Execution complete'
+  if [ -f  ./../howard/wwwroot/scripts/pi.py ]; then
+    echo 'Wating for script file change'
+    inotifywait -qe close_write ./../howard/wwwroot/scripts/pi.py
+    echo 'Wait complete. Executing command'
+    sudo python ./../howard/wwwroot/scripts/pi.py
+    echo 'Execution complete'
+  else
+    echo 'File is missing...'
+    sleep 1
+  fi
 done
 
